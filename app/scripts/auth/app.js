b6b4149c0eba0c32b31777d09ff07f4dc20c0f35
We3x <veljko.simic98@gmail.com>
2015-03-11 23:22:20 +0100

    Creates login controller

--- app/scripts/auth/app.js
+++ app/scripts/auth/app.js
@@ -8,6 +8,8 @@ angular.module('Liberator.auth',[
 
   .config(require('./config'))
 
+  .controller('loginControler', require('./controller.js'))
+
   .service('loginService', require('./service.js'))
 
   .run(require('./run.js'));
