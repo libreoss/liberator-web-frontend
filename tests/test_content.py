8da913b7c6342007fac913a084a993181879624d
Stefan Nožinić <stefan@lugons.org>
2016-07-30 23:11:06 +0200

    Fix create test and add content test

--- tests/test_content.py
+++ tests/test_content.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 from time import sleep
 
-class TestArticleCreate(unittest.TestCase):
+class TestContent(unittest.TestCase):
     
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -21,54 +21,47 @@ class TestArticleCreate(unittest.TestCase):
         submit = self.driver.find_element_by_tag_name("button")
         submit.click()
         sleep(2)
+        
+        elem = self.driver.find_element_by_link_text("Create article")
+        elem.click()
+        sleep(2)
+        sections = self.driver.find_element_by_id("section-selector")
+        sections.find_elements_by_tag_name("button")[0].click()
+
+        authors = self.driver.find_element_by_id("author-selector")
+        authors.find_elements_by_tag_name("button")[0].click()
+
+        self.driver.find_element_by_id("submit").click()
+        sleep(2)
+        self.driver.find_element_by_link_text("List articles").click()
+        sleep(2)
+        self.driver.find_element_by_class_name("section-select-box").find_elements_by_tag_name("a")[1].click()
+        sleep(15)
+        self.article_item = self.driver.find_elements_by_class_name("article_item")[-1]
     
     def tearDown(self):
         self.driver.quit()
 
-    def test_fields_exist(self):
-        elem = None
-        try:
-            elem = self.driver.find_element_by_link_text("Create article")
-        except NoSuchElementException:
-            self.fail("Menu item not found")
-
-    def test_create_with_one_author_no_issue(self):
-        elem = self.driver.find_element_by_link_text("Create article")
-        elem.click()
+    def test_content_add(self):
         sleep(2)
-        sections = self.driver.find_element_by_id("section-selector")
-        sections.find_elements_by_tag_name("button")[0].click()
-
-        authors = self.driver.find_element_by_id("author-selector")
-        authors.find_elements_by_tag_name("button")[0].click()
-
+        self.article_item.find_element_by_class_name("dropdown-toggle").click()
+        sleep(5)
+        self.article_item.find_element_by_class_name("dropdown-menu").find_element_by_tag_name("a").click()
+        sleep(2)
+        title = self.driver.find_element_by_class_name("input-group").find_element_by_tag_name("input")
+        title.send_keys("Content add test title")
+        editor = self.driver.find_element_by_class_name("editor-editable")
+        editor.send_keys("This is content")
+        self.driver.find_element_by_class_name("dropdown-toggle").click()
+        sleep(2)
+        self.driver.find_element_by_id("state-list").find_element_by_tag_name("a").click()
         self.driver.find_element_by_id("submit").click()
         sleep(2)
+
         self.driver.find_element_by_link_text("List articles").click()
         sleep(2)
         self.driver.find_element_by_class_name("section-select-box").find_elements_by_tag_name("a")[1].click()
         sleep(2)
+        self.article_item = self.driver.find_elements_by_class_name("article_item")[-1]
 
-        self.assertGreater(len(self.driver.find_elements_by_class_name("article_item")), 0)
-    
-    def test_create_with_one_author_one_issue(self):
-        elem = self.driver.find_element_by_link_text("Create article")
-        elem.click()
-        sleep(2)
-        sections = self.driver.find_element_by_id("section-selector")
-        sections.find_elements_by_tag_name("button")[0].click()
-
-        authors = self.driver.find_element_by_id("author-selector")
-        authors.find_elements_by_tag_name("button")[0].click()
-        
-        issues = self.driver.find_element_by_id("issue-selector")
-        issues.find_elements_by_tag_name("button")[0].click()
-
-        self.driver.find_element_by_id("submit").click()
-        sleep(2)
-        self.driver.find_element_by_link_text("List articles").click()
-        sleep(2)
-        self.driver.find_element_by_class_name("section-select-box").find_elements_by_tag_name("a")[1].click()
-        sleep(2)
-
-        self.assertGreater(len(self.driver.find_elements_by_class_name("article_item")), 0)
+        self.assertEqual(len(self.article_item.find_element_by_class_name("content-list").find_elements_by_tag_name("a")), 2)
