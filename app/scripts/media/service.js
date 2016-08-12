01e3ecf96f6eac4ace8780e1a5533c6ebcacd2bb
Stefan Nožinić <stefan@lugons.org>
2016-08-12 19:00:36 +0200

    Media listing

--- app/scripts/media/service.js
+++ app/scripts/media/service.js
@@ -16,8 +16,14 @@ module.exports = [
         headers: {'Content-Type': undefined}
       });
     }
+    function getMedia(article) 
+    {
+      console.log("Getting media for " + article.id);
+      return $http.get(API_URL + "articles/ " + article.id + "/media/");
+    }
     return {
       upload: upload,
+      getMedia: getMedia,
     };
 
 }]
